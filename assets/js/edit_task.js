622eb1b9d2deebbb4261adcd96ec1a3f960a2769
Fairy Princess <keepgoindgdev007@gmail.com>
2018-11-21 20:06:36 +1000

    edit finish

--- assets/js/edit_task.js
+++ assets/js/edit_task.js
@@ -1,8 +1,8 @@
 $(document).on('click', '#btn-update', function(e){
     e.preventDefault();
     tasksTestApi.updateTaskContent($(this).attr('data-id'), $("#task-title").val(), $("#task-descripton").val()).done(function(data){
-
         console.log(data);
+        window.location.href = "/";
     }).fail(function(err){
         console.log(err);
     }).always(function(){
